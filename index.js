d8218dd4060e3931309fcb53d176e2c11c80bb25
James Pickering <james_pic@hotmail.com>
2018-03-30 16:13:03 +0100

    Get defaultAccount from window.web3.eth.defaultAccount - need to figure out whether this is necessary

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import contract from 'truffle-contract'
 import cloud from 'wordcloud'
-import Web3 from 'web3'
 
 document.addEventListener('DOMContentLoaded', (event) => {
   const WordCloud = contract(require('./build/contracts/WordCloud.json'))
   const provider = window.web3.currentProvider
-  const web3 = new Web3(provider)
-  WordCloud.setProvider(window.web3.currentProvider)
+  WordCloud.setProvider(provider)
 
   let words = {}
   let updated = false
@@ -51,7 +49,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('submitButton').addEventListener('click', (event) => {
       let message = document.getElementById('newWord').value
       let value = Math.floor(parseFloat(document.getElementById('value').value) * 10 ** 18)
-      wordCloud.increaseWordSize(message, {value: value, from: web3.eth.defaultAccount})
+      wordCloud.increaseWordSize(message, {value: value, from: ((window.web3 || {}).eth || {}).defaultAccount})
     })
   })
 
