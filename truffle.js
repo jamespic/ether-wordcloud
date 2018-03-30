d9d165f02444d90ef0e953ced927f0d0578920df
James Pickering <james_pic@hotmail.com>
2018-03-30 14:35:54 +0100

    First deployed to mainnet

--- truffle.js
+++ truffle.js
@@ -13,25 +13,22 @@ module.exports = {
         let createLedgerSubprovider = require("@ledgerhq/web3-subprovider").default
         let TransportHID = require("@ledgerhq/hw-transport-node-hid").default
         let ProviderEngine = require("web3-provider-engine")
-        let RpcSubprovider = require("web3-provider-engine/subproviders/rpc")
+        let FetchSubprovider = require("web3-provider-engine/subproviders/fetch")
         let engine = new ProviderEngine();
-        let getTransport = () => {
-          console.log('Getting transport')
-          return result
-          let result = TransportU2F.create()
-        };
-        let ledger = createLedgerSubprovider(getTransport, {
-          accountsOffset: 1,
-          accountsLength: 5
-        });
-        engine.addProvider(ledger);
-        engine.addProvider(new RpcSubprovider({rpcUrl: 'http://127.0.0.1:8545'}));
-        engine.start();
-        engine.sendAsync({id: 1, jsonrpc: '2.0', method: 'net_version', params: []}, (err, res) => {console.error(err), console.log(res)})
+        let getTransport = () => {return TransportHID.create()}
+        let ledger = createLedgerSubprovider(getTransport, {accountsLength: 10})
+        //var logger = Object.create(require('web3-provider-engine/subproviders/subprovider').prototype)
+        //logger.handleRequest = function(payload, next, end) {console.log(payload);next()}
+        //engine.addProvider(logger)
+        engine.addProvider(ledger)
+        engine.addProvider(new FetchSubprovider({rpcUrl: 'http://127.0.0.1:8545'}))
+        engine.start()
+        engine.sendAsync({id: 1, jsonrpc: '2.0', method: 'net_version', params: []}, (err, res) => {console.error(err); console.log(res)})
         return engine
       },
       network_id: '1',
-      gasPrice: 1000000000
+      gasPrice: 2000000000,
+      from: '0x758E53a86224F6511DBcabd9A364E21B4689653F'
     }
   }
 };
